102a0897dc01196dd0eb4c3346c085d5177164ae
Maximiliano Delgado <mdelgado@baufest.com>
2021-01-17 16:52:26 -0300

    chore: edit test

--- test/character.spec.js
+++ test/character.spec.js
@@ -5,9 +5,14 @@ const app  = require('../src/app');
 const serverConfig = config['server'];
 
 describe('Endpoint Character', () => {
-  it('Should be return all characters', async () => {
+  it('Should be return 200 and all characters', async () => {
     const result = await request(app).get(`${serverConfig['context']}/character`).send();
     expect(result.status).toBe(200);
     expect(result.body).not.toBeEmpty();
+  });
+
+  it('Should dont be return status 200', async () => {
+    const result = await request(app).get(`${serverConfig['context']}/character?name=asdsa`).send();
+    expect(result.status).not.toBe(200);
   });
 });
